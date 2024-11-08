5180f3bdb8940226afdb7dd283bd65411c058602
damontoth <Damon.Toth@hsc.utah.edu>
2024-11-08 15:40:35 -0700

    Added another test, for Model 3

--- tests/testthat/test-facilityR0.R
+++ tests/testthat/test-facilityR0.R
@@ -14,7 +14,6 @@ test_that("facilityR0() works for Model 1", {
   expect_equal(facilityR0(S = 0, C = 0, A = 1, transm = bet, initS = 1, mgf = mgf), R0exact, tolerance = sqrt(.Machine$double.eps))
 })
 
-
 test_that("facilityR0() works for Model 2", {
   prob <- c(0.58, 1-0.58)
   rate <- c(0.0285, 0.179)
@@ -32,4 +31,25 @@ test_that("facilityR0() works for Model 2", {
   R0exact <- bet / gam * (1 - K(-gam) / mgf(0,1))
 
   expect_equal(facilityR0(S = 0, C = -gam, A = 1, transm = bet, initS = 1, mgf = mgf), R0exact, tolerance = sqrt(.Machine$double.eps))
+})
+
+test_that("facilityR0() works for Model 3", {
+  prob <- c(0.58, 1-0.58)
+  rate <- c(0.0285, 0.179)
+  shape <- c(1, 5.74)
+  bet <- 0.051
+  gam <- 0.0026
+  dc <- 0.00845
+  eps <- 0.55
+
+  MGFmixedgamma <- function(x, prob, rate, shape, deriv=0)
+    sum(exp(log(prob)+lgamma(shape+deriv)-lgamma(shape)-shape*log(1-x/rate)-deriv*log(rate-x)))
+
+  mgf <- function(x, deriv=0) MGFmixedgamma(x, prob, rate, shape, deriv)
+
+  K <- function(x) (mgf(x)-1)/x
+
+  R0exact <- bet/(dc+gam)*((1-dc*(1-eps)/(dc+gam))*(1-K(-dc-gam)/mgf(0,1))+dc*(1-eps)*mgf(0,2)/mgf(0,1)/2)
+
+  expect_equal(facilityR0(S = 0, C = rbind(c(-dc-gam,0),c(dc,0)), A = rbind(1,0), transm = bet*c(1,1-eps), initS = 1, mgf = mgf), R0exact, tolerance = sqrt(.Machine$double.eps))
 })
