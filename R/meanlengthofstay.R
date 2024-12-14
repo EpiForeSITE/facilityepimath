20e9e7d0f53bfadf2a5a14a4c618d8bb112246ca
damontoth <Damon.Toth@hsc.utah.edu>
2024-12-13 21:37:27 -0700

    Added meanlengthofstay function and documentation

---
+++ R/meanlengthofstay.R
@@ -0,0 +1,22 @@
+#' Calculate the mean length of stay for a linear facility model
+#'
+#' @param M A matrix of state transition rates between facility patient states
+#' @param init A vector of admission state probabilities to each state
+#' @param mgf The moment generating function characterizing a time-of-stay-dependent removal hazard
+#' @return The mean length of stay
+#' @export
+meanlengthofstay <- function(M, init, mgf){
+  if(is.null(mgf)){
+    return(sum(as.vector(solve(M,init))))
+  }
+  if(all(abs(colSums(M))<.Machine$double.eps)){
+    return(mgf(0,1))
+  }
+  K <- function(x, deriv = 0)
+    ifelse(x == 0, mgf(0, deriv+1)/(deriv+1), ifelse(deriv == 0, (mgf(x)-1)/x, (mgf(x, deriv) - deriv * K(x, deriv-1))/x))
+  K <- Vectorize(K,'x')
+  eig <- eigen(M)
+  solveV <- solve(eig$vectors,init)
+  Klamb <- K(eig$values)
+  sum(as.vector(eig$vectors %*% (solveV * Klamb)))
+}
