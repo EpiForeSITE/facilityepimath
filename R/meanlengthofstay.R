13806080de94d5d7814c48ffebdba130cd8d0cc2
damontoth <Damon.Toth@hsc.utah.edu>
2025-01-08 17:48:55 -0700

    Added example documentation to meanlengthofstay

--- R/meanlengthofstay.R
+++ R/meanlengthofstay.R
@@ -4,6 +4,11 @@
 #' @param init A vector of admission state probabilities to each state
 #' @param mgf The moment generating function characterizing a time-of-stay-dependent removal hazard
 #' @return The mean length of stay
+#' @examples
+#' M <- rbind(c(-1.1,2),c(1,-2.2))
+#' init <- c(0.9,0.1)
+#' mgf <- function(x, deriv=0) MGFgamma(x, rate=0.2, shape=3, deriv)
+#' meanlengthofstay(M, init, mgf)
 #' @export
 meanlengthofstay <- function(M, init, mgf){
   if(is.null(mgf)){
