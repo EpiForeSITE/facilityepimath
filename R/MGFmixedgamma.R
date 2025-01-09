d532e55ea172ac65765982abd6580766026d8591
damontoth <Damon.Toth@hsc.utah.edu>
2025-01-08 17:29:40 -0700

    Added example documentation to MGF functions

--- R/MGFmixedgamma.R
+++ R/MGFmixedgamma.R
@@ -6,6 +6,11 @@
 #' @param shape A vector of shape parameter values for each gamma distribution in the mixture
 #' @param deriv An integer, the number of derivatives of the MGF to apply
 #' @return The number resulting from the function evaluation
+#' @examples
+#' # MGF of a 40/60 mixture of two gamma distributions, evaluated at -0.1:
+#' MGFmixedgamma(-0.1, prob = c(0.4,0.6), rate = c(0.4,0.7), shape = c(0.5,3))
+#' # Second moment of the distribution (second derivative evaluated at zero):
+#' MGFmixedgamma(0, prob = c(0.4,0.6), rate = c(0.4,0.7), shape = c(0.5,3), deriv = 2)
 #' @export
 MGFmixedgamma <- function(x, prob, rate, shape, deriv=0)
   sum(exp(log(prob)+lgamma(shape+deriv)-lgamma(shape)-shape*log(1-x/rate)-deriv*log(rate-x)))
